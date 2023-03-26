0862116649ed20527de2fdeada4e181c1911bd29
songxxzp <songxxzp@163.com>
2023-03-26 16:17:01 +0800

    Add gpt model

---
+++ gpt.py
@@ -0,0 +1,50 @@
+import torch
+
+from torch.nn import CrossEntropyLoss
+from typing import List
+from torch import tensor
+from torch.nn.functional import softmax
+from transformers import BertTokenizerFast, AutoModel, AutoTokenizer, GPT2Config, BertTokenizer, GPT2LMHeadModel, TextGenerationPipeline
+
+
+def load_gpt_model(device="cpu"):
+    # model = GPT2LMHeadModel.from_pretrained("ckiplab/gpt2-tiny-chinese").to(device)
+    # tokenizer = BertTokenizerFast.from_pretrained('bert-base-chinese')
+    tokenizer = BertTokenizer.from_pretrained("uer/gpt2-chinese-cluecorpussmall")
+    model = GPT2LMHeadModel.from_pretrained("uer/gpt2-chinese-cluecorpussmall").to(device)
+
+    return model, tokenizer
+
+
+def build_tokenized_pinyin_dict(pinyin_dict, tokenizer, device="cpu"):
+    tokenized_pinyin_dict = {}
+    for pinyin, token_list in pinyin_dict.items():
+        token_id_list = tensor([tokenizer.convert_tokens_to_ids(c) for c in token_list], device=device)
+        tokenized_pinyin_dict[pinyin] = token_id_list
+
+
+def gpt_probability(prefix, token, pinyin, model, tokenizer, tokenized_pinyin_dict):
+    input_ids = tokenizer(prefix, return_tensors='pt').to(model.device)
+    logits = model(**input_ids).logits
+    prob_list = softmax(logits[0][-2], dim=-1)
+
+    total_prob = prob_list[tokenized_pinyin_dict[pinyin]].sum()
+
+    token_id = tokenizer.convert_tokens_to_ids(token)
+
+    return float(prob_list[token_id] / total_prob)
+
+
+def perplexity(seq: List[str], model, tokenizer):
+    input_ids = tokenizer(seq, return_tensors='pt', padding=True).to(model.device)
+    logits = model(**input_ids).logits
+
+    shift_logits = logits[:, :-1, :].contiguous()
+    shift_labels = input_ids['input_ids'][:, 1:].contiguous()
+    shift_attentions = input_ids['attention_mask'][:, 1:].contiguous()
+
+    loss_fn = CrossEntropyLoss(ignore_index=0, reduction="none")
+    loss = loss_fn(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)).detach().reshape(len(seq), -1)
+    meanloss = loss.sum(1) / shift_attentions.sum(1)
+    ppl = torch.exp(meanloss).tolist()
+    return ppl
--- main.py
+++ main.py
@@ -8,27 +8,64 @@ from functools import partial
 from config import *
 from model import *
 from metric import *
+from gpt import *
 
 
 para_lambda = 0.03
-top_k = 10
+top_k_storage = 10
+top_k_calculate = 1
 max_conditional_prefix_length = 2
 
 
-def laplace_smoothing(conditional_frequency_dict, prefix_frequency_dict, token, prefix="", vocab_num=6763):
+def laplace_smoothing(conditional_frequency_dict, prefix_frequency_dict, token, pinyin=None, prefix="", vocab_num=6763):
+    """
+        laplace smoothing
+        return p(token|prefix)
+    """
     probability = (get_conditional_frequency(conditional_frequency_dict, prefix, token) + 1) / (get_frequency(prefix_frequency_dict, prefix) + vocab_num)
     return probability
 
 
-def interpolation_smoothing(conditional_probabilistic_dict, token, prefix=""):
-    """ probability smoothing """
+def interpolation_smoothing(conditional_probabilistic_dict, token, pinyin=None, prefix=""):
+    """
+        interpolation smoothing
+        return p(token|prefix)
+    """
     # if len(prefix) > 1:
     #     return get_conditional_probability(probabilistic_dict, prefix, token) * (1 - para_lambda) + smoothing_probability(probabilistic_dict, prefix[-1:], token) * para_lambda
     probability = get_conditional_probability(conditional_probabilistic_dict, prefix, token) * (1 - para_lambda) + get_conditional_probability(conditional_probabilistic_dict, "", token) * para_lambda
     return probability
 
 
-def pinyin_to_character(pinyin_str: str, smoothing, pinyin_dict, conditional_probabilistic_dict, max_conditional_prefix_length=1, top_k=1):
+def normalized_probability(pinyin_dict, probability_fn, token, pinyin, prefix=""):
+    """
+        normalized probability
+        return p(token|prefix, pinyin)
+    """
+    probability = probability_fn(token=token, prefix=prefix)
+    totall_probability = sum([probability_fn(token=token, prefix=prefix) for token in pinyin_dict[pinyin]])
+    return probability / totall_probability
+
+
+def default_top_k_selector(seq_list: List[str]) -> str:
+    """
+        choose one answer from a list
+        return seq_list[0] as default
+    """
+    return seq_list[0]
+
+
+def gpt_top_k_selector(seq_list: List[str], model, tokenizer) -> str:
+    """
+        choose one answer from a list
+        return seq with minima PPL
+    """
+    ppl = perplexity(seq_list, model, tokenizer)
+    seq = seq_list[ppl.index(min(ppl))]
+    return seq
+
+
+def pinyin_to_character(pinyin_str: str, get_probability, pinyin_dict, max_conditional_prefix_length=1, top_k_storage=1, top_k_calculate=1, top_k_selector=default_top_k_selector):
     pinyin_list = pinyin_str.rstrip("\n").split(" ")
     if len(pinyin_list) < 1:
         return ""
@@ -38,74 +75,87 @@ def pinyin_to_character(pinyin_str: str, smoothing, pinyin_dict, conditional_pro
         pinyin = pinyin_list[i]
         for token in pinyin_dict[pinyin]:
             if i == 0:
-                search_state[i][token] = [(smoothing(token=token), "")]
+                search_state[i][token] = [(get_probability(token=token, prefix="", pinyin=pinyin), "")]
             else:
                 for pre_token, max_prefix_prob_list in search_state[i - 1].items():
                     for j in range(len(max_prefix_prob_list)):
                         max_prefix_prob = max_prefix_prob_list[-j][0]
-                        prefix = (max_prefix_prob_list[-j][1] + pre_token)[-max_conditional_prefix_length:]
-                        seq_prob = max_prefix_prob * smoothing(token=token, prefix=prefix)  # TODO: use log
+                        prefix = (max_prefix_prob_list[-j][1] + pre_token)
+                        seq_prob = max_prefix_prob * get_probability(token=token, prefix=prefix[-max_conditional_prefix_length:], pinyin=pinyin)  # TODO: use log
                         if token not in search_state[i]:
                             search_state[i][token] = [(seq_prob, prefix)]
                         else:
                             search_state[i][token].append((seq_prob, prefix))
 
             search_state[i][token].sort(key=lambda x: x[0])
-            search_state[i][token] = search_state[i][token][- top_k : ]
+            search_state[i][token] = search_state[i][token][- top_k_storage : ]
 
     # get last token with highst probability
-    last_token = ""
-    max_prob = -1e-8
-    for token, max_token_prob_list in search_state[-1].items():
-        max_token_prob = max_token_prob_list[-1][0]
-        if max_token_prob > max_prob:
-            max_prob = max_token_prob
-            last_token = token
-    
-    # track last token
-    sentence = ""
-    for i in reversed(range(len(pinyin_list))):
-        sentence = last_token + sentence
-        max_token_prob_list = search_state[i][last_token]
-        last_token = max_token_prob_list[-1][1]
-        if len(last_token) > 1:
-            last_token = last_token[-1]
 
-    return sentence
+    final_search_state = []
+    for token, last_token_prob_list in search_state[-1].items():
+        for token_prob_tuple in last_token_prob_list:
+            final_search_state.append((token_prob_tuple[0], token_prob_tuple[1] + token))
+
+    final_search_state.sort(key=lambda x: x[0], reverse=True)
+    final_search_state = final_search_state[:top_k_storage]
+
+    # track last token
+    sentences = []
+    for _, sentence in final_search_state:
+        sentences.append(sentence)
+
+    # print(sentences)
+
+    return top_k_selector(seq_list=sentences)
 
 
 def main():
     vocabs, pinyin_dict = load_vocab()
-    conditional_frequency_dict = load_frequency_dict(vocabs=vocabs)
-    prefix_frequency_dict = load_prefix_frequency_dict(conditional_frequency_dict)
     conditional_probabilistic_dict = load_probabilistic_model(vocabs=vocabs)
 
-    print("model loaded")
-
-    smoothing = partial(
+    probability_fn = partial(
         interpolation_smoothing,
         conditional_probabilistic_dict=conditional_probabilistic_dict
     )
 
-    # smoothing = partial(
+    # model, tokenizer = load_gpt_model(device="cuda")
+    # selector = partial(
+    #     gpt_top_k_selector,
+    #     model=model,
+    #     tokenizer=tokenizer
+    # )
+
+    # conditional_frequency_dict = load_frequency_dict(vocabs=vocabs)
+    # prefix_frequency_dict = load_prefix_frequency_dict(conditional_frequency_dict)
+    # probability_fn = partial(
     #     laplace_smoothing,
     #     conditional_frequency_dict=conditional_frequency_dict,
     #     prefix_frequency_dict=prefix_frequency_dict,
     #     vocab_num=len(vocabs)
     # )
 
+    get_probability = probability_fn
+
+    # get_probability = partial(
+    #     normalized_probability,
+    #     pinyin_dict=pinyin_dict,
+    #     probability_fn=probability_fn
+    # )
+
     process_fn = partial(
         pinyin_to_character,
-        smoothing=smoothing,
+        get_probability=get_probability,
         pinyin_dict=pinyin_dict,
-        conditional_probabilistic_dict=conditional_probabilistic_dict,
         max_conditional_prefix_length=min(max_conditional_prefix_length, max_prefix_length),
-        top_k=top_k
+        top_k_storage=top_k_storage,
+        top_k_calculate=top_k_calculate,
+        # top_k_selector=selector
     )
 
     for (input_file_path, input_format), (output_file_path, output_format) in zip(input_path, output_path):
         with open(input_file_path, "r", encoding=input_format) as input_file:
-            answers = map(process_fn, input_file.readlines())
+            answers = list(map(process_fn, input_file.readlines()))
         with open(output_file_path, "w", encoding=output_format) as output_file:
             answers = [answer + '\n' for answer in answers]
             output_file.writelines(answers)
