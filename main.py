12e3e47b6c7f11827ed297528ea38e5c1404b1ee
songxxzp <songxxzp@163.com>
2023-03-27 18:49:47 +0800

    Refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from functools import partial
 from config import load_config
 
 load_config(
-    model_path="/home/song/workspace/pinyin/trigram_weibo_newscrawl_baike",  # trigram_weibo_newscrawl_baike, trigram_newscrawl_baike, bigram_weibo
+    model_path="/home/song/workspace/pinyin/bigram_weibo",  # trigram_weibo_newscrawl_baike, trigram_newscrawl_baike, bigram_weibo
     input_file_path="/home/song/下载/拼音输入法作业-2023春/测试语料/input.txt",
     std_file_path="/home/song/下载/拼音输入法作业-2023春/测试语料/std_output.txt",
     output_file_path="/home/song/workspace/pinyin/output.txt"
@@ -43,29 +43,27 @@ def pinyin_to_character(pinyin_str: str, probability_fn, pinyin_dict, max_condit
         pinyin = pinyin_list[i]
         for token in pinyin_dict[pinyin]:
             if i == 0:
-                search_state[token] = [(probability_fn(token=token, prefix="", pinyin=pinyin), "")]
+                search_state[token] = [(probability_fn(token=token, prefix="", pinyin=pinyin), token)]
             else:
                 search_state[token] = []
-                for pre_token, last_token_prob_list in last_search_state.items():
-                    for token_prob_tuple in last_token_prob_list[:top_k_calculate]:
-                        max_prefix_prob = token_prob_tuple[0]
-                        prefix = token_prob_tuple[1] + pre_token
-                        seq_prob = max_prefix_prob * probability_fn(token=token, prefix=prefix[-max_conditional_prefix_length:], pinyin=pinyin)  # TODO: use log
-                        search_state[token].append((seq_prob, prefix))
+                for _, last_token_prob_list in last_search_state.items():
+                    for max_prefix_prob, prefix in last_token_prob_list[:top_k_calculate]:  # (prob, prefix)
+                        seq_prob = max_prefix_prob * probability_fn(token=token, prefix=prefix[-max_conditional_prefix_length:], pinyin=pinyin)
+                        search_state[token].append((seq_prob, prefix + token))
 
             search_state[token].sort(key=lambda x: x[0], reverse=True)
             search_state[token] = search_state[token][: top_k_storage]
 
         last_search_state = search_state
 
-    # get last token with top k probability
+    # get sentences with top k probability
     final_search_state = []
-    for token, last_token_prob_list in last_search_state.items():
-        for token_prob_tuple in last_token_prob_list[:top_k_calculate]:
-            final_search_state.append((token_prob_tuple[0], token_prob_tuple[1] + token))
+    for _, last_token_prob_list in last_search_state.items():
+        final_search_state += last_token_prob_list[:top_k_calculate]
 
     final_search_state.sort(key=lambda x: x[0], reverse=True)
     final_search_state = final_search_state[:top_k_storage]
+
     sentences = [sentence for _, sentence in final_search_state]  # sort from high prob to low prob
 
     return top_k_selector(seq_list=sentences)
