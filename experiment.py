fbe59b567300fd82956f7061f4a16fe570ec2469
songxxzp <songxxzp@163.com>
2023-04-02 12:56:17 +0800

    Fix experiment.py

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 import os
+import datetime
 
 from args import get_parser
 
@@ -26,13 +27,15 @@ if __name__ == "__main__":
     result_dict = {}
     result_list = []
 
+    start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     for model_path in model_path_list:
         for top_k in top_k_list:
             for final_selector in final_selector_list:
                 output_file_path = os.path.join(results_path, os.path.basename(model_path) + ".txt")
                 args.model_path = model_path
-                args.calculate_top_k_selector = top_k
-                args.storage_top_k_selector = top_k
+                args.top_k_storage = top_k
+                args.top_k_calculate = top_k
                 args.final_top_k_selector = final_selector
                 args.output_file_path = output_file_path
                 args.max_conditional_prefix_length = 2
@@ -43,8 +46,8 @@ if __name__ == "__main__":
                 result_dict[(model_path, top_k, final_selector)] = (time_usage, sentence_acc, word_acc)
                 result_list.append([model_path, top_k, final_selector, time_usage, sentence_acc, word_acc])
 
-    with open(os.path.join(results_path, "results.json"), "w", encoding="utf-8") as file:
-        json.dump(result_list, file)
+        with open(os.path.join(results_path, "results {}.json".format(start_time)), "w", encoding="utf-8") as file:
+            json.dump(result_list, file)
 
-    with open(os.path.join(results_path, "results.pkl"), "wb") as file:
-        pickle.dump(result_dict, file)
+        with open(os.path.join(results_path, "results {}.pkl".format(start_time)), "wb") as file:
+            pickle.dump(result_dict, file)
